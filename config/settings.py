e2d0d278c6f6e9f2c67342711dbd2ab6b302dde5
tetsuya <satoh1529@gmail.com>
2021-07-10 16:22:22 +0900

    adeed export default

--- config/settings.py
+++ config/settings.py
@@ -146,6 +146,7 @@ AUTH_USER_MODEL = 'accounts.CustomUser'
 CORS_ALLOWED_ORIGINS = (
     'https://project-hangout.vercel.app/',
 )
+CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
 
 # allauth設定
